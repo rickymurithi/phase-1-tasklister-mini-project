5c3c907a473560b8da1ec2ace885e6a833ec2086
erickmuriithi <erick.muriithi@student.moringaschool.com>
2022-09-06 08:56:38 +0300

    add file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // code!
+  form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  myTodoList(e.target.newTask.value)
+  form.reset()
+ })
 });
+
+function buildTodoList(todo) {
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  li.textContent = `${todo}`
+  li.appendChild(btn)
+  document.querySelector('#tasks').appendChild(li)
+}
+function handleDelete (e) {
+  e.target.parentNode.remove()
+}
